8f6d8885aa9196ab650e6a8f8f966bae0f8687be
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-02-25 17:28:22 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ export default {
   ],
   theme: {
     extend: {
-
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"]
+      }
     },
   },
   plugins: [],
