c0c65c9e56dd6336026d2d4b58aa39e4a6951da5
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-02-26 08:19:24 +0100

    add gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,21 @@ import { logo } from "../assets"
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-        <nav className="flex w-full justify-between items-center mb-10 pt-3">
-            <img src={logo} alt="sumz-logo" className="w-28 object-contain"/>
+      <nav className="flex w-full justify-between items-center mb-10 pt-3">
+        <img src={logo} alt="sumz-logo" className="w-28 object-contain"/>
+        <button 
+          type="button" 
+          onClick={() => {window.open('https://github.com/...')}}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
 
-            <button 
-                type="button" 
-                onClick={() => {window.open('https://github.com/...')}}
-                className="black_btn"
-            >
-                    Github
-            </button>
-
-        </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
     </header>
   )
 }
