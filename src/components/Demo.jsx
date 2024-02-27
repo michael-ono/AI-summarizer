3f4a1dd371f351afe2ac6dcd63b63363bd930c47
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-02-27 03:58:07 +0100

    add button

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
   return (
-    <div>Demo</div>
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form action=""
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" />
+
+          <input 
+              type="text" 
+              placeholder="Enter a url"
+              value=""
+              onChange={() => {}}
+              required
+              className="url_input peer"
+            />
+        </form>
+
+      </div>
+    </section>
   )
 }
 
