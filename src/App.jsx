4baf408551990feb67ff1e7c3113c48b693a3848
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-04-13 17:44:44 +0100

    import navigate from react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import "./App.css";
+import Signup from "./components/Signup";
 
 const App = () => {
     return (
         <div>
             <Routes>
                 <Route path="/" exact element={<Home />}></Route>
-                <Route path="/signup" element={<SignUp />}></Route> 
+                <Route path="/signup" element={<Signup />}></Route> 
             </Routes>
         </div>
     )
